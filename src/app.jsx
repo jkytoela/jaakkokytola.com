bf5a38ad01dc56f1d04239ebd1118c09f179f4cd
Jaakko Kytölä <kytola.jaakko@gmail.com>
2021-03-02 09:37:05 +0200

    build(redirects): #2 attempt to fix routing error on Netlify

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import { Route, Switch,  } from 'wouter';
+import { Route, Switch, Redirect } from 'wouter';
 import Index from './domain/Index';
 import Home from './domain/Home';
 
 const App = () => (
   <Switch>
-    <Route path="/" component={Index} />
     <Route path="/home" component={Home} />
+    <Route path="/" component={Index} />
   </Switch>
 );
 
